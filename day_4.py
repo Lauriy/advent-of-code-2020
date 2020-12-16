b1c9e43da0452b72b807b2e1f5f8b7331912609d
Lauri Elias <laurileet@gmail.com>
2020-12-16 11:38:22 +0200

    Commit day 4, fix off-by-one

--- day_4.py
+++ day_4.py
@@ -1,3 +1,4 @@
+import re
 import typing
 from dataclasses import dataclass, field
 
@@ -14,16 +15,40 @@ class Passport:
     cid: typing.Optional[int] = field(default=None)
 
     def validate(self):
-        print(self)
         for each in ('byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'):
-            if getattr(self, each) is None:
-                print(each)
+            if not getattr(self, each):
                 return False
 
         return True
 
+    def validate_2(self):
+        if not self.validate():
+            return False
+        if not 1920 <= self.byr <= 2002:
+            return False
+        if not 2010 <= self.iyr <= 2020:
+            return False
+        if not 2020 <= self.eyr <= 2030:
+            return False
+        if not self.hgt.endswith('cm') and not self.hgt.endswith('in'):
+            return False
+        height_digits = re.findall(r'\d+', self.hgt)
+        height_number = int(height_digits[0])
+        if self.hgt.endswith('in') and not 59 <= height_number <= 76:
+            return False
+        elif self.hgt.endswith('cm') and not 150 <= height_number <= 193:
+            return False
+        if self.hcl[0] != '#' or not self.hcl[1:].isalnum():
+            return False
+        if self.ecl not in ('amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'):
+            return False
+        if len(self.pid) != 9:
+            return False
 
-def solve_first(file_name: str) -> int:
+        return True
+
+
+def solve_both(file_name: str) -> (int, int):
     with open(file_name, 'r') as f:
         passport_data = f.readlines()
     passports = []
@@ -36,14 +61,17 @@ def solve_first(file_name: str) -> int:
             continue
         line_data = line.strip().split(' ')
         for item in line_data:
-            key_value = item.split(':')
+            key_value = item.strip().split(':')
             if key_value[0] in ('byr', 'iyr', 'eyr', 'cid'):
                 key_value[1] = int(key_value[1])
             setattr(passport, key_value[0], key_value[1])
 
     valid_count = 0
+    valid_2_count = 0
     for each in passports:
         if each.validate():
             valid_count += 1
+        if each.validate_2():
+            valid_2_count += 1
 
-    return valid_count
+    return valid_count, valid_2_count
--- test_day_4.py
+++ test_day_4.py
@@ -1,6 +1,7 @@
-from day_4 import solve_first
+from day_4 import solve_both
 
 
 def test_solve_first():
-    assert solve_first('day_4_sample.txt') == 2
-    assert solve_first('day_4_my_input.txt') == 216
+    assert solve_both('day_4_sample.txt') == (2, 2)
+    # 290 total
+    assert solve_both('day_4_my_input.txt') == (216, 150)
