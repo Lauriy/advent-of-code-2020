cc797d6071ac029436af17bcb575f40429c7ac83
Lauri Elias <laurileet@gmail.com>
2020-12-17 12:26:02 +0200

    Commit day 5

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # AoC 2020
 My Python solutions. Use pytest to run 'em.
+
+```bash
+python3.9 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+pytest
+```
---
+++ day_5.py
@@ -0,0 +1,37 @@
+def _parse_single_string(boarding_pass: str) -> (int, int):
+    start = 0
+    end = 127
+    for char in boarding_pass[:7]:
+        if char == 'F':
+            end = int((start + end + 1) / 2) - 1
+        elif char == 'B':
+            start = int((start + end + 1) / 2)
+
+    row = start
+    start = 0
+    end = 7
+
+    for char in boarding_pass[7:]:
+        if char == 'L':
+            end = int((start + end + 1) / 2) - 1
+        elif char == 'R':
+            start = int((start + end + 1) / 2)
+
+    column = start
+
+    return row, column
+
+
+def solve_first(boarding_passes: list) -> int:
+    rows_cols = [_parse_single_string(x) for x in boarding_passes]
+    ids = [x[0] * 8 + x[1] for x in rows_cols]
+
+    return max(ids)
+
+
+def solve_second(boarding_passes: list) -> int:
+    rows_cols = [_parse_single_string(x) for x in boarding_passes]
+    ids = [x[0] * 8 + x[1] for x in rows_cols]
+    all_possible_ids = range(min(ids), max(ids))
+
+    return [x for x in all_possible_ids if x not in ids][0]
